05e575a732a70fb6e28290ecf09cf9b99d1379bb
Sylvain059 <sylvain.sculier88@gmail.com>
2017-09-14 13:53:26 +0200

    minichat_post.php / add comment

--- minichat_post.php
+++ minichat_post.php
@@ -9,7 +9,7 @@
 
 <body>
     <?php
-// Connexion à la base de données
+    /* Connexion à la base de données*/
 try
 {
     $bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', 'Skaoune88');
@@ -19,11 +19,11 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
-// Insertion du message à l'aide d'une requête préparée
+    /* Requête préparé */
 $req = $bdd->prepare('INSERT INTO minichat (pseudo, message) VALUES(?, ?)');
 $req->execute(array($_POST['pseudo'], $_POST['message']));
 
-// Redirection du visiteur vers la page du minichat
+    /* Redirection de l'utilisateur vers la page minichat.php */
 header('Location: minichat.php');
 ?>
 
